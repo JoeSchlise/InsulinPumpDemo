f06b6a00bbe1001514b2e868a5ef2fc46a6e0274
JoeSchlise <148167548+JoeSchlise@users.noreply.github.com>
2025-02-06 13:15:08 -0600

    Add files via upload

--- InsulinOutline.py
+++ InsulinOutline.py
@@ -2,7 +2,8 @@
 '''look at the graphic of all the elements necessary for the project'''
 '''read from file to get blood and write back with updated blood sugar levels'''
 '''whenever there is an update in the blood sugar write to a file with the date and time and the amount of insulin'''
-
+'''read textbook for examples or sanarios and see how timing plays into dose amount'''
+'''we also need to gather statistics about the person, those can either be entered by us or put in the text file'''
 def blood_sugar_calc() :
 
 
@@ -13,6 +14,8 @@ def test_blood():
 def store_result():
 
 def dispense_insulin():
+
+def persons_attributes():
 
 
 
