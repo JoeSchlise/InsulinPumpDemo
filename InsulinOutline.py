cc2e728238d71b43de4e2e3e9c74a7d7976e4ff8
JoeSchlise <148167548+JoeSchlise@users.noreply.github.com>
2025-02-06 11:23:03 -0600

    Starter outline of what im thinking for the project

---
+++ InsulinOutline.py
@@ -0,0 +1,18 @@
+'''learn how to read a file in python and begin outlining the porject'''
+'''look at the graphic of all the elements necessary for the project'''
+'''read from file to get blood and write back with updated blood sugar levels'''
+'''whenever there is an update in the blood sugar write to a file with the date and time and the amount of insulin'''
+
+def blood_sugar_calc() :
+
+
+def read_blood():
+
+def test_blood():
+
+def store_result():
+
+def dispense_insulin():
+
+
+
