85876d6236edf60cd6412a4128d9d218027b82fc
Nick Spieker <124298136+Leaf45@users.noreply.github.com>
2025-02-09 13:14:17 -0600

    Update InsulinOutline.py
    Comments outlining all the functions

--- InsulinOutline.py
+++ InsulinOutline.py
@@ -4,17 +4,23 @@
 '''whenever there is an update in the blood sugar write to a file with the date and time and the amount of insulin'''
 '''read textbook for examples or sanarios and see how timing plays into dose amount'''
 '''we also need to gather statistics about the person, those can either be entered by us or put in the text file'''
+
+# mathmatical calculation to be called by test_blood()
 def blood_sugar_calc() :
 
-
+# read data from body.txt which simulates blood from consumer
 def read_blood():
 
+# interpret data from read_blood() and pass it to dispense_insulin()
 def test_blood():
 
+# store change make to the blood stream in updated_blood.txt
 def store_result():
 
+# dispense insulin based on results from test_blood()
 def dispense_insulin():
 
+# individuals 'account' --> name, birthday, etc.
 def persons_attributes():
 
 
