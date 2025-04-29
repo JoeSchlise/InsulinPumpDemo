6fb8509f57318e85253866f68802f87177d74ffe
JoeSchlise <148167548+JoeSchlise@users.noreply.github.com>
2025-04-29 11:38:05 -0500

    Update Login.py

--- Login.py
+++ Login.py
@@ -2,29 +2,27 @@ from tinydb import TinyDB, Query
 
 db = TinyDB('db_for_pump.json')
 
-def create_account():
-    name = input("Enter a Username: ")
-    password = input("Enter a password: ")
-    db.insert({'username': name, 'password': password, 'file_name' : name+password})
+def create_account(new_username, new_password):
+    db.insert({'username': new_username, 'password': new_password, 'file_name' : new_username+new_password})
 
 
 def error():
     print("Wrong credentials entered")
 
-def login():
-    name = input("Enter your username: ")
-    password = input("Enter your password: ")
+def login(username, password):
 
     table = db.table('_default')
     User = Query()
 
-    user_temp = table.search(User.username == name)
+    user_temp = table.search(User.username == username)
     pass_temp = table.search(User.password == password)
 
     if user_temp == [] or pass_temp == []:
         error()
+        return False
     else:
         print("Logging in")
+        return True
         #set results file to be their results file
 
 
