f72e2c66ed851caea70c4ffe053aaf087da3753c
Nick Spieker <124298136+Leaf45@users.noreply.github.com>
2025-04-29 18:57:21 -0500

    Update Login.py
    added dynamic file generation and assignment for whoever is logged in

--- Login.py
+++ Login.py
@@ -3,29 +3,36 @@ from tinydb import TinyDB, Query
 db = TinyDB('db_for_pump.json')
 
 def create_account(new_username, new_password):
-    db.insert({'username': new_username, 'password': new_password, 'file_name' : new_username+new_password})
+    db.insert({'username': new_username, 'password': new_password, 'file_name' : new_username+new_password + ".txt"})
+    t_file = open(new_username+new_password + ".txt", "x")
+    t_file.close()
 
 
 def error():
     print("Wrong credentials entered")
 
 def login(username, password):
-
+    print("test")
     table = db.table('_default')
     User = Query()
 
-    user_temp = table.search(User.username == username)
-    pass_temp = table.search(User.password == password)
+    user = table.get((User.username == username) & (User.password == password))
 
-    if user_temp == [] or pass_temp == []:
+    if user is None:
         error()
-        return False
+        return False, None
     else:
-        print("Logging in")
-        return True
-        #set results file to be their results file
+        print("Logged in as:", user['username'])
+        print("User doc_id:", user.doc_id)  # Access the unique document ID
+        return True, get_file(user.doc_id)
 
-
+def get_file(id):
+    table = db.table('_default')
+    doc = table.get(doc_id=id)  # assuming user_id = 1
+    if doc:
+        file_name = doc['file_name']
+        print("User's file name:", file_name)
+        return file_name
 
 #create_account()
 #login()
